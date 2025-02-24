9dea9f3e52c476f0595c6e2298c28ff6443e2bb1
santoshhbg799 <59023966+santoshhbg799@users.noreply.github.com>
2025-02-24 13:29:19 +0530

    Add files via upload

---
+++ rag-gist-2-imports/rag-gist-2-imports/ingestion.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+if __name__ == '__main__':
+    print("Ingesting...")
+
